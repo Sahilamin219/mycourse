3984119866d92bec099826414a04f1662e356b47
Sahil Amin <Sahilamin219@gmail.com>
2025-10-25 20:22:34 +0530

    Updated useWebRTC.ts

--- src/hooks/useWebRTC.ts
+++ src/hooks/useWebRTC.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:8001';
+const SOCKET_URL = 'https://mycourse-32jb.onrender.com/';
 
 const ICE_SERVERS = {
   iceServers: [
